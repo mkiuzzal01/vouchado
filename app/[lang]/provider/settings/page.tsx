a381527d1f47f8895f81b50b29762b452174ac88
mkiuzzal01 <mkiuzzal07@gmail.com>
2026-07-12 16:48:34 +0600

    update the provider settings

--- app/[lang]/provider/settings/page.tsx
+++ app/[lang]/provider/settings/page.tsx
@@ -1,11 +1,9 @@
 import BusinessAnalytics from "../__components/BusinessAnalytics";
-import DealList from "../__components/DealList";
 
 export default function page() {
   return (
     <div>
       <BusinessAnalytics />
-      <DealList />
     </div>
   );
 }
